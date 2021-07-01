18500de7a828388da34b6eeec91b2c459db83081
StasyanOi <iluvstrwrs@mail.ru>
2021-07-02 00:20:12 +0300

    created setup

--- setup.sh
+++ setup.sh
@@ -11,3 +11,16 @@ mkdir mask_overlay_datasets/CelebA-HQ-img-256-256-merged
 
 unzip CelebA-HQ-img-256-256.zip -d mask_overlay_datasets
 
+sudo apt install python3.7
+python3.7 -m virtualenv env
+source env/bin/activate
+pip install -r requirments.txt
+
+# After the environment is setup - run 'source env/bin/activate'
+# to activate the environment in the terminal and then
+# python <filename.py> to run a script or map the environment
+# in an IDE that you are using
+
+
+
+
